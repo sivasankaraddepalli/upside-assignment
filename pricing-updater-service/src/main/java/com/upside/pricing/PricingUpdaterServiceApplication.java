6e716715d1429f6d0aa2c12f0c922b87f5a0e086
sivashankaraddepalli <sivashankaraddepalli1994@gmail.com>
2022-05-27 09:55:27 +0530

    initial commit

---
+++ pricing-updater-service/src/main/java/com/upside/pricing/PricingUpdaterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upside.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingUpdaterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingUpdaterServiceApplication.class, args);
+	}
+
+}
---
+++ pricing-updater-service/src/test/java/com/upside/pricing/PricingUpdaterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upside.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingUpdaterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
