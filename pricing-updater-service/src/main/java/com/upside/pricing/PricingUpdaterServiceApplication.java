529c86b43359abfd99dc86fa094587fa772d2ded
sivasankaraddepalli <sivasankaraddepalli1994@gmail.com>
2022-05-27 10:17:19 +0530

    test

--- pricing-updater-service/src/main/java/com/upside/pricing/PricingUpdaterServiceApplication.java
+++ pricing-updater-service/src/main/java/com/upside/pricing/PricingUpdaterServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PricingUpdaterServiceApplication {
 
 	public static void main(String[] args) {
+
 		
 		SpringApplication.run(PricingUpdaterServiceApplication.class, args);
 	}
